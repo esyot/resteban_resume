3b2f8d77d9b91fea633427d65fec75d1e698c7c3
rei.nhard3367@gmail.com <rei.nhard3367@gmail.com>
2024-09-06 08:18:07 +0800

    First commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Reinhard U. Esteban';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'A fresh graduate from Mater Dei College, Cabulijan, Tubigon, Bohol of school year 2024-2025.';
+export const ABOUT_TEXT_2 =
+  'Has a wide background of PHP, and other frameworks.';
+export const ABOUT_TEXT_3 =
+  'Capable of building complete web applications from start to finish. Possess a strong understanding of various programming languages, frameworks, and databases.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'Fresh graduate in web development from Mater Dei College, having completed my studies in the 2024-2025 school year.';
+export const RESUME_TEXT_2 =
+  'Fresh web development graduate from Mater Dei College, class of 2024, with in-depth knowledge of PHP, Java, and JavaScript.';
